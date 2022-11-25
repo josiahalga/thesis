29d0962460317c4cfa5d52b6f5b2cb6733574ff5
Josiah <josiahalga@gmail.com>
2022-11-25 17:44:04 +0800

    Changed the algorithm for GUI

--- traffic.py
+++ traffic.py
@@ -1,20 +1,12 @@
 class lane:
     def __init__(self):
-        self.red = False
-        self.yellow = False
-        self.green = False
+        self.state = 1
     
     def switch_red(self):
-        self.red = True
-        self.yellow = False
-        self.green = False
+        self.state = 1
      
     def switch_yellow(self):
-        self.yellow = True
-        self.red = False
-        self.green = False
+        self.state = 2
         
     def switch_green(self):
-        self.green = True
-        self.red = False
-        self.yellow = False
+        self.state = 3
--- traffic_cycle.py
+++ traffic_cycle.py
@@ -170,12 +170,9 @@ def main():
     while True: #cycle through 4 lanes
         lane_1_green()
         display_lanes()
-        green_light_1.color('green')
         sleep(5)
         lane_1_yellow()
         display_lanes()
-        yellow_light_1.color('yellow')
-        green_light_1.color('grey')
         sleep(3)
         lane_2_green()
         display_lanes()
@@ -196,86 +193,84 @@ def main():
         display_lanes()
         sleep(3)
         
-    wn.mainloop()
+    wn.mainloop() 
         
 #function for displaying text
 def display_lanes():
         
     colorama.init(autoreset=True)
     
-    print(Fore.GREEN + 'Lane 1 Green:' + Fore.GREEN, lane_1.green, end=' ')
-    print(Fore.GREEN + '\tLane 2 Green:', lane_2.green, end=' ')
-    print(Fore.GREEN + '\tLane 3 Green:', lane_3.green, end=' ')
-    print(Fore.GREEN + '\tLane 4 Green:', lane_4.green)
-    print(Fore.YELLOW + 'Lane 1 Yellow:', lane_1.yellow, end=' ')
-    print(Fore.YELLOW + '\tLane 2 Yellow', lane_2.yellow, end=' ')
-    print(Fore.YELLOW + '\tLane 3 Yellow', lane_3.yellow, end=' ')
-    print(Fore.YELLOW + '\tLane 4 Yellow', lane_4.yellow)
-    print(Fore.RED + 'Lane 1 Red:', lane_1.red, end=' ')
-    print(Fore.RED + '\tLane 2 Red:', lane_2.red, end=' ')
-    print(Fore.RED + '\tLane 3 Red:', lane_3.red, end=' ')
-    print(Fore.RED + '\tLane 4 Red:', lane_4.red)
+    print(Fore.GREEN + 'Lane 1 Green:' + Fore.GREEN, lane_1.state, end=' ')
+    print(Fore.GREEN + '\tLane 2 Green:', lane_2.state, end=' ')
+    print(Fore.GREEN + '\tLane 3 Green:', lane_3.state, end=' ')
+    print(Fore.GREEN + '\tLane 4 Green:', lane_4.state)
+    print(Fore.YELLOW + 'Lane 1 Yellow:', lane_1.state, end=' ')
+    print(Fore.YELLOW + '\tLane 2 Yellow', lane_2.state, end=' ')
+    print(Fore.YELLOW + '\tLane 3 Yellow', lane_3.state, end=' ')
+    print(Fore.YELLOW + '\tLane 4 Yellow', lane_4.state)
+    print(Fore.RED + 'Lane 1 Red:', lane_1.state, end=' ')
+    print(Fore.RED + '\tLane 2 Red:', lane_2.state, end=' ')
+    print(Fore.RED + '\tLane 3 Red:', lane_3.state, end=' ')
+    print(Fore.RED + '\tLane 4 Red:', lane_4.state)
     print('------------------------------------------')
     
-def traffic_1_green():
-    red_light_1.color('grey')
-    yellow_light_1.color('grey')
-    green_light_1.color('green')
-
-def traffic_1_yellow():
-    red_light_1.color('grey')
-    yellow_light_1.color('yellow')
-    green_light_1.color('grey')
+def traffic_1_check():
     
-def traffic_1_red():
-    red_light_1.color('red')
-    yellow_light_1.color('grey')
-    green_light_1.color('grey')
+    if lane_1.state == 3:
+        red_light_1.color('grey')
+        yellow_light_1.color('grey')
+        green_light_1.color('green')
+    elif lane_1.state == 2:
+        red_light_1.color('grey')
+        yellow_light_1.color('yellow')
+        green_light_1.color('grey')
+    elif lane_1.state == 1:
+        red_light_1.color('red')
+        yellow_light_1.color('grey')
+        green_light_1.color('grey')
     
-def traffic_2_green():
-    red_light_2.color('grey')
-    yellow_light_2.color('grey')
-    green_light_2.color('green')
+def traffic_2_check():
+    if lane_2.state == 3:
+        red_light_2.color('grey')
+        yellow_light_2.color('grey')
+        green_light_2.color('green')
+    elif lane_2.state == 2:
+        red_light_2.color('grey')
+        yellow_light_2.color('yellow')
+        green_light_2.color('grey')
+    elif lane_2.state == 1:
+        red_light_2.color('red')
+        yellow_light_2.color('grey')
+        green_light_2.color('grey')
 
-def traffic_2_yellow():
-    red_light_2.color('grey')
-    yellow_light_2.color('yellow')
-    green_light_2.color('grey')
-
-def traffic_2_red():
-    red_light_2.color('red')
-    yellow_light_2.color('grey')
-    green_light_2.color('grey')
     
-def traffic_3_green():
-    red_light_3.color('grey')
-    yellow_light_3.color('grey')
-    green_light_3.color('green')
-
-def traffic_3_yellow():
-    red_light_3.color('grey')
-    yellow_light_3.color('yellow')
-    green_light_3.color('grey')
-
-def traffic_3_red():
-    red_light_3.color('red')
-    yellow_light_3.color('grey')
-    green_light_3.color('grey')
+def traffic_3_check():
+    if lane_3.state == 3:
+        red_light_3.color('grey')
+        yellow_light_3.color('grey')
+        green_light_3.color('green')
+    elif lane_3.state == 2:
+        red_light_3.color('grey')
+        yellow_light_3.color('yellow')
+        green_light_3.color('grey')
+    elif lane_3.state == 1:
+        red_light_3.color('red')
+        yellow_light_3.color('grey')
+        green_light_3.color('grey')
     
-def traffic_4_green():
-    red_light_4.color('grey')
-    yellow_light_4.color('grey')
-    green_light_4.color('green')
-
-def traffic_4_yellow():
-    red_light_4.color('grey')
-    yellow_light_4.color('yellow')
-    green_light_4.color('grey')
-
-def traffic_4_red():
-    red_light_4.color('red')
-    yellow_light_4.color('grey')
-    green_light_4.color('grey')
+def traffic_4_check():
+    if lane_4.state == 3:
+        red_light_4.color('grey')
+        yellow_light_4.color('grey')
+        green_light_4.color('green')
+    elif lane_4.state == 2:
+        red_light_4.color('grey')
+        yellow_light_4.color('yellow')
+        green_light_4.color('grey')
+    elif lane_4.state == 1:
+        red_light_4.color('red')
+        yellow_light_4.color('grey')
+        green_light_4.color('grey')
 
 #The following functions are for switching the lane status
 def lane_1_green():
@@ -283,30 +278,33 @@ def lane_1_green():
     lane_2.switch_red()
     lane_3.switch_red()
     lane_4.switch_red()
-    traffic_1_green()
-    traffic_2_red()
-    traffic_3_red()
-    traffic_4_red()
+    
+    traffic_1_check()
+    traffic_2_check()
+    traffic_3_check()
+    traffic_4_check()
     
 def lane_1_yellow():
     lane_1.switch_yellow()
     lane_2.switch_red()
     lane_3.switch_red()
     lane_4.switch_red()
-    traffic_1_yellow()
-    traffic_2_red()
-    traffic_3_red()
-    traffic_4_red()
+    
+    traffic_1_check()
+    traffic_2_check()
+    traffic_3_check()
+    traffic_4_check()
     
 def lane_2_green():
     lane_1.switch_red()
     lane_2.switch_green()
     lane_3.switch_red()
     lane_4.switch_red()
-    traffic_1_red()
-    traffic_2_green()
-    traffic_3_red()
-    traffic_4_red()
+    
+    traffic_1_check()
+    traffic_2_check()
+    traffic_3_check()
+    traffic_4_check()
     
     
 def lane_2_yellow():
@@ -314,30 +312,33 @@ def lane_2_yellow():
     lane_2.switch_yellow()
     lane_3.switch_red()
     lane_4.switch_red()
-    traffic_1_red()
-    traffic_2_yellow()
-    traffic_3_red()
-    traffic_4_red()
+    
+    traffic_1_check()
+    traffic_2_check()
+    traffic_3_check()
+    traffic_4_check()
     
 def lane_3_green():
     lane_1.switch_red()
     lane_2.switch_red()
     lane_3.switch_green()
     lane_4.switch_red()
-    traffic_1_red()
-    traffic_2_red()
-    traffic_3_green()
-    traffic_4_red()
+    
+    traffic_1_check()
+    traffic_2_check()
+    traffic_3_check()
+    traffic_4_check()
     
 def lane_3_yellow():
     lane_1.switch_red()
     lane_2.switch_red()
     lane_3.switch_yellow()
     lane_4.switch_red()
-    traffic_1_red()
-    traffic_2_red()
-    traffic_3_yellow()
-    traffic_4_red()
+    
+    traffic_1_check()
+    traffic_2_check()
+    traffic_3_check()
+    traffic_4_check()
     
 def lane_4_green():
     lane_1.switch_red()
@@ -345,10 +346,10 @@ def lane_4_green():
     lane_3.switch_red()
     lane_4.switch_green()
     
-    traffic_1_red()
-    traffic_2_red()
-    traffic_3_red()
-    traffic_4_green()
+    traffic_1_check()
+    traffic_2_check()
+    traffic_3_check()
+    traffic_4_check()
     
 def lane_4_yellow():
     lane_1.switch_red()
@@ -356,10 +357,10 @@ def lane_4_yellow():
     lane_3.switch_red()
     lane_4.switch_yellow()
     
-    traffic_1_red()
-    traffic_2_red()
-    traffic_3_red()
-    traffic_4_yellow()
+    traffic_1_check()
+    traffic_2_check()
+    traffic_3_check()
+    traffic_4_check()
     
 if __name__ == "__main__":
     main()
